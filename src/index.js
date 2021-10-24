b27efe9c52907c21612931cfacf62389deb22479
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-10-24 17:19:00 -0500

    Added bootstrap to index.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Popper from "popper.js";
-import bootstrap from "bootstrap";
 import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
