fb434204ee9009fbc4c4b25b3b31fe188ae15cce
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-11-02 16:42:44 -0500

    Added Netlify link to Footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,10 @@ export default function Footer() {
         <a href="https://github.com/tasialedet/TCodes-weather-app">
           Open Source Code 💻 on GitHub
         </a>{" "}
-        by Tasia Ledet and hosted on <a href="">Netlify</a>
+        by Tasia Ledet and hosted on{" "}
+        <a href="https://zealous-pare-87110b.netlify.app" target="blank">
+          Netlify
+        </a>
       </p>
     </div>
   );
