8dfc85c4bb79dfb910f1ec7431e7fb2b3d99cf6d
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-11-02 16:27:28 -0500

    Removed unit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,47 +2,10 @@ import React, { useState } from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("imperial");
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("metric");
-  }
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("imperial");
-  }
-  if (unit === "imperial") {
-    return (
-      <strong className="temp">
-        {Math.round(props.temp)}
-        <span className="units">
-          <a href="/" onClick={showFahrenheit} className="fahrenheit">
-            °F{" "}
-          </a>
-          |{" "}
-          <a href="/" onClick={showCelsius} className="celsius">
-            {" "}
-            °C{" "}
-          </a>{" "}
-        </span>
-      </strong>
-    );
-  } else {
-    let celsius = props.temp - (32 * 5) / 9;
-    return (
-      <strong className="temp">
-        {Math.round(celsius)}
-        <span className="units">
-          <a href="/" onClick={showFahrenheit} className="fahrenheit">
-            °F{" "}
-          </a>
-          |{" "}
-          <a href="/" onClick={showCelsius} className="celsius">
-            {" "}
-            °C{" "}
-          </a>{" "}
-        </span>
-      </strong>
-    );
-  }
+  return (
+    <strong className="temp">
+      {Math.round(props.temp)}
+      <span className="units">°F </span>
+    </strong>
+  );
 }
