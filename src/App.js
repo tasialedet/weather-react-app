efb3d5786184978522c76d16ae2dae2004d205d6
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-11-01 23:26:39 -0500

    Removed Forecast import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather";
-
-import Forecast from "./Forecast";
 import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="weatherApp">
       <Weather defaultCity="Houston" />
-      
+
       <Footer />
     </div>
   );
